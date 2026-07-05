3ed5ce74e016b8adc527f1fe46a1994123d63808
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-05 12:49:31 +0530

    Time: 0 ms (100%), Space: 31.1 MB (71.43%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n=nums.size();
+        int val=nums[n/2];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val) count++;
+        }
+        if(count>1) return false;
+        return true;
+    }
+};
