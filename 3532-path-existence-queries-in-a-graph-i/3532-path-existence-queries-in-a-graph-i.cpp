b24e94943e811b012aeac155bce4cf110116f033
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-09 13:34:55 +0530

    Time: 0 ms (100%), Space: 228.8 MB (55.07%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -23,18 +23,21 @@ public:
                 }
             }
         }
+        int id=0;
+        vector<int> segmentID(n);
+        for(int i=0;i<segments.size();i++){
+            int l=segments[i].first;
+            int r=segments[i].second;
+            for(int j=l;j<=r;j++){
+                segmentID[j]=id;
+            }
+            id++;
+        }
         vector<bool> ans(queries.size(),false);
         for(int i=0;i<queries.size();i++){
             int l=min(queries[i][0],queries[i][1]);
             int r=max(queries[i][0],queries[i][1]);
-
-            auto it = upper_bound(segments.begin(),segments.end(),make_pair(l,INT_MAX));
-            if(it==segments.begin()){
-                ans[i]=false;
-            } else {
-                --it;
-                ans[i]=it->second>=r;
-            }
+            ans[i]=segmentID[l]==segmentID[r];
         }
         return ans;
     }
